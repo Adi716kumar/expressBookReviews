dd4425be50456d372e7f788d38c344fc310f58ac
Aditya Kumar <adityasrivastava716@gmail.com>
2026-02-21 05:24:00 -0500

    Task 13: Get books by title using async await with Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,48 +38,37 @@ public_users.get('/', async function (req, res) {
   });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
   
-    if (books[isbn]) {
-      return res.status(200).json(books[isbn]);
-    } else {
+    try {
+      const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+      return res.status(200).json(response.data);
+    } catch (error) {
       return res.status(404).json({ message: "Book not found" });
     }
   });
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
-    let matchingBooks = {};
   
-    Object.keys(books).forEach(isbn => {
-      if (books[isbn].author === author) {
-        matchingBooks[isbn] = books[isbn];
-      }
-    });
-  
-    if (Object.keys(matchingBooks).length > 0) {
-      return res.status(200).json(matchingBooks);
-    } else {
+    try {
+      const response = await axios.get(`http://localhost:5000/author/${author}`);
+      return res.status(200).json(response.data);
+    } catch (error) {
       return res.status(404).json({ message: "No books found for this author" });
     }
   });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title;
-    let matchingBooks = {};
   
-    Object.keys(books).forEach(isbn => {
-      if (books[isbn].title === title) {
-        matchingBooks[isbn] = books[isbn];
-      }
-    });
-  
-    if (Object.keys(matchingBooks).length > 0) {
-      return res.status(200).json(matchingBooks);
-    } else {
+    try {
+      const response = await axios.get(`http://localhost:5000/title/${title}`);
+      return res.status(200).json(response.data);
+    } catch (error) {
       return res.status(404).json({ message: "No books found with this title" });
     }
   });
