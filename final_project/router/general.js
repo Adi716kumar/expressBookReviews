0cb98bb204cf4705140b88644589a3ac17785e73
Aditya Kumar <adityasrivastava716@gmail.com>
2026-02-21 00:12:36 -0500

    task 3,4,5 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,21 +27,50 @@ public_users.get('/isbn/:isbn', function (req, res) {
   });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    let matchingBooks = {};
+  
+    Object.keys(books).forEach(isbn => {
+      if (books[isbn].author === author) {
+        matchingBooks[isbn] = books[isbn];
+      }
+    });
+  
+    if (Object.keys(matchingBooks).length > 0) {
+      return res.status(200).json(matchingBooks);
+    } else {
+      return res.status(404).json({ message: "No books found for this author" });
+    }
+  });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+    let matchingBooks = {};
+  
+    Object.keys(books).forEach(isbn => {
+      if (books[isbn].title === title) {
+        matchingBooks[isbn] = books[isbn];
+      }
+    });
+  
+    if (Object.keys(matchingBooks).length > 0) {
+      return res.status(200).json(matchingBooks);
+    } else {
+      return res.status(404).json({ message: "No books found with this title" });
+    }
+  });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+  
+    if (books[isbn]) {
+      return res.status(200).json(books[isbn].reviews);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  });
 
 module.exports.general = public_users;
